759c7f61d90be5238c58f6696aec8e95ee62b428
illiantech <pimentel2002d@gmail.com>
2024-08-21 17:26:42 -0400

    like animation

--- src/components/ContentModal.tsx
+++ src/components/ContentModal.tsx
@@ -20,14 +20,15 @@ export const Content = ({ info, title, children }: Props) => {
       <h3 class="flex items-center px-6 py-6 text-base font-bold lg:h-[15%] lg:text-xl">
         <div class="w-5/6 lg:w-full">{title}</div>
       </h3>
-      <div class="h-1/2 overflow-auto border-y border-neutral-700 p-6 lg:absolute lg:top-[15%] lg:h-4/6">
+      <div class="h-1/2 overflow-y-scroll border-y border-neutral-700 p-6 lg:absolute lg:top-[15%] lg:h-4/6">
         <p> {info.description}</p>
         <br />
         <br />
-
-        {comments.map((comment) => {
-          return <Comment key={comment.id} {...comment} />;
-        })}
+        <section>
+          {comments.map((comment) => {
+            return <Comment key={comment.id} {...comment} />;
+          })}
+        </section>
       </div>
       <FormComment setComments={setComments} />
     </article>
