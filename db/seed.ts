cded646d9434e9a2e801356db34489c129a0588f
illiantech <pimentel2002d@gmail.com>
2024-08-27 08:36:06 -0400

    pp

--- db/seed.ts
+++ db/seed.ts
@@ -1,5 +1,16 @@
 import { db, Sites } from "astro:db";
 
+// fetch(
+//   "https://raw.githubusercontent.com/illiantech/spa_colonia_tovar/main/JSON/tourismSites.json"
+// )
+//   .then((res) => res.json())
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log("errooor");
+//   });
+
 // https://astro.build/db/seed
 export default async function seed() {
   await db.insert(Sites).values([{ id: "sdsd" }, { id: "sdsdsds" }]);
